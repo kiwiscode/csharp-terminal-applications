8a3caf8e939d65391a7e4994095a18cd0d8e602f
kiwiscode <ayktkav@gmail.com>
2025-09-24 00:10:33 +0200

    Initialize global variables for username, taskList, and exitApp

--- 0-Todo App/Globals.cs
+++ 0-Todo App/Globals.cs
@@ -9,4 +9,10 @@ public static class Globals
     // List to store users
     public static List<User> UserList { get; set; } = new();
 
+    // List to store tasks
+    public static List<_Task> TaskList { get; set; } = new();
+
+    public static string? username = "";
+    public static bool exitApp = false;
+
 }
