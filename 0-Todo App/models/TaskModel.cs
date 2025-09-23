6258fff9bf64cb1e1ee0f85f0098461019f60be7
kiwiscode <ayktkav@gmail.com>
2025-09-24 00:14:49 +0200

    add task model

---
+++ 0-Todo App/models/TaskModel.cs
@@ -0,0 +1,13 @@
+
+
+
+namespace todo_app.Models
+{
+    public class _Task
+    {
+        public string? Username { get; set; } = "";
+        public string Description { get; set; } = "";
+        public string Category { get; set; } = "";
+        public string Status { get; set; } = "";
+    }
+}
